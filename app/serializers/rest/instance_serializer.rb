5f37c3a6b57bd666f992734328b4db2aa1929c5a
KKKKKENWAY <albuskenway@gmail.com>
2021-06-15 04:42:39 +0000

    word_limit

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -5,7 +5,7 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
-             :languages, :registrations, :approval_required, :invites_enabled
+             :languages, :registrations, :approval_required, :max_toot_chars, :invites_enabled
 
   has_one :contact_account, serializer: REST::AccountSerializer
 
@@ -61,6 +61,10 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   def approval_required
     Setting.registrations_mode == 'approved'
+  end
+  
+  def max_toot_chars
+      10000
   end
 
   def invites_enabled
